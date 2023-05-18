e4ebbd352248d3c9206a714078a59de40576d172
Ujjwal Dahiya <ujjwaldahiya350@gmail.com>
2023-05-18 11:30:15 +0530

    helloWorld

---
+++ src/ClassCompHelloWorld.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css'
+
+class ClassCompHelloWorld extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Class component is saying Hello World</h1>
+            </div>
+        )
+    }
+}
+export default ClassCompHelloWorld;
